c15e7db7c48ec2201d1142c52ed312a79c5ecb12
Carolina Muñoz <caromz2304@gmail.com>
2025-12-30 13:46:51 -0300

    Decimoprimer commit: "Cambio de alert a sweetAlert"

--- js/main.js
+++ js/main.js
@@ -9,20 +9,22 @@ const treatments = [
   { name: "Capping gel", price: 25000 },
   { name: "Manicura semipermanente", price: 20000 },
   { name: "Depilación de rostro", price: 12000 },
-  { name: "Depilación de piernas", price: 14000 }
+  { name: "Depilación de piernas", price: 14000 },
 ];
 
-const selectForm = document.getElementById('options');
+const selectForm = document.getElementById("options");
 
 selectForm.innerHTML += treatments
-  .map(treatement => `<option value="${treatement.name}">${treatement.name} - $${treatement.price}</option>`)
-  .join('');
-
+  .map(
+    (treatement) =>
+      `<option value="${treatement.name}">${treatement.name} - $${treatement.price}</option>`
+  )
+  .join("");
 
 //Shifts list
 const asignedShifts = JSON.parse(localStorage.getItem("shifts")) || [];
 
-const showAsignedShifts = document.getElementById(`list-shifts`)
+const showAsignedShifts = document.getElementById(`list-shifts`);
 
 function renderShifts() {
   if (asignedShifts.length === 0) {
@@ -42,7 +44,9 @@ function renderShifts() {
         </thread>
         <tbody>`;
 
-  tableHtml += asignedShifts.map((shift,index) => `
+  tableHtml += asignedShifts
+    .map(
+      (shift, index) => `
     <tr>
       <td>${shift.name} ${shift.lastname}</td>
       <td>${shift.treatment}</td>
@@ -52,82 +56,130 @@ function renderShifts() {
         <button class="btn-velvet" id="modify" onclick="editShift(${index})">Modificar</button>
         <button class="btn-velvet" id="delete" onclick="deleteShift(${index})">Eliminar</button>
       </td>
-    </tr>`).join('');
+    </tr>`
+    )
+    .join("");
 
   tableHtml += `
       </tbody>
     </table>`;
 
-  showAsignedShifts.innerHTML = tableHtml;}
+  showAsignedShifts.innerHTML = tableHtml;
+}
 
 //Delete shift
-window.deleteShift = function(index) {
-  if (confirm("¿Estás seguro de que deseas eliminar este turno?")) {
-    asignedShifts.splice(index, 1);
-    localStorage.setItem("shifts", JSON.stringify(asignedShifts)); 
-    renderShifts(); 
-  }
+window.deleteShift = function (index) {
+  Swal.fire({
+    title: "¿Estás seguro?",
+    text: "Una vez eliminado el turno no podrás recuperarlo.",
+    icon: "warning",
+    iconColor: "#ecab0f",
+    showCancelButton: true,
+    confirmButtonColor: "#f8878f",
+    cancelButtonColor: "#f8a9ae",
+    confirmButtonText: "Eliminar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      asignedShifts.splice(index, 1);
+      localStorage.setItem("shifts", JSON.stringify(asignedShifts));
+      renderShifts();
+      Swal.fire({
+        title: "Eliminado",
+        text: "El turno ha sido borrado con éxito.",
+        icon: "success",
+        confirmButtonColor: "#f8a9ae",
+        confirmButtonText: "Entendido",
+      });
+    }
+  });
 };
 
 //Modify shift
-window.editShift = function(index) {
+window.editShift = function (index) {
   const shift = asignedShifts[index];
 
   asignedShifts.splice(index, 1);
 
-  document.getElementById('name').value = shift.name;
-  document.getElementById('lastname').value = shift.lastname;
-  document.getElementById('options').value = shift.treatment;
-  document.getElementById('date').value = shift.date;
-  document.getElementById('time').value = shift.time;
+  document.getElementById("name").value = shift.name;
+  document.getElementById("lastname").value = shift.lastname;
+  document.getElementById("options").value = shift.treatment;
+  document.getElementById("date").value = shift.date;
+  document.getElementById("time").value = shift.time;
 
-  
-  alert("Los datos se han cargado en el formulario. Realiza los cambios y presiona 'Reservar turno' para guardar.");
+  Swal.fire({
+    title: "Modo Edición",
+    text: "Los datos se han cargado en el formulario. Realiza los cambios y presiona 'Reservar turno'.",
+    icon: "info",
+    iconColor: "#ecab0f",
+    confirmButtonColor: "#f8a9ae",
+    confirmButtonText: "Entendido",
+  });
 };
 
 renderShifts();
 
 //Validación de la fecha
-const dateInput = document.getElementById('date');
+const dateInput = document.getElementById("date");
 if (dateInput) {
-    const today = new Date().toISOString().split('T')[0];
-    dateInput.setAttribute('min', today);
+  const today = new Date().toISOString().split("T")[0];
+  dateInput.setAttribute("min", today);
 }
 
 //Save form
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
-form.addEventListener('submit', function(event){
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const name = document.getElementById('name').value.trim();
-  const lastname = document.getElementById('lastname').value.trim();
-  const treatment = document.getElementById('options').value;
-  const date = document.getElementById('date').value;
-  const time = document.getElementById('time').value;
+  const name = document.getElementById("name").value.trim();
+  const lastname = document.getElementById("lastname").value.trim();
+  const treatment = document.getElementById("options").value;
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
 
   //Validaciones del formulario
   if (!name || !lastname || !treatment || !date || !time) {
-    alert("Por favor, completa todos los campos.");
+    Swal.fire({
+      title: "Error",
+      text: "Todos los campos del formulario deben ser completados.",
+      confirmButtonColor: "#f8a9ae",
+      confirmButtonText: "Entendido",
+    });
     return;
   }
 
   const soloLetras = /^[a-zA-ZÁÉÍÓÚáéíóúÑñ\s]+$/;
   if (!soloLetras.test(name)) {
-    alert("El nombre solo puede contener letras.");
-    return; 
+    Swal.fire({
+      title: "Error",
+      text: "El nombre solo puede contener letras.",
+      confirmButtonColor: "#f8a9ae",
+      confirmButtonText: "Entendido",
+    });
+    return;
   }
   if (!soloLetras.test(lastname)) {
-    alert("El apellido solo puede contener letras.");
-    return; 
+    Swal.fire({
+      title: "Error",
+      text: "El apellido solo puede contener letras.",
+      confirmButtonColor: "#f8a9ae",
+      confirmButtonText: "Entendido",
+    });
+    return;
   }
 
-  const isDuplicate = asignedShifts.some(shift => 
-    shift.date === date && shift.time === time
+  const isDuplicate = asignedShifts.some(
+    (shift) => shift.date === date && shift.time === time
   );
 
   if (isDuplicate) {
-    alert("Lo sentimos, este horario ya está reservado. Por favor elige otro.");
+    Swal.fire({
+      title: "Error",
+      text: "El turno ya se encuentra ocupado.",
+      confirmButtonColor: "#f8a9ae",
+      confirmButtonText: "Entendido",
+    });
     return;
   }
 
@@ -136,7 +188,7 @@ form.addEventListener('submit', function(event){
     lastname: lastname,
     treatment: treatment,
     date: date,
-    time: time
+    time: time,
   };
 
   asignedShifts.push(newShift);
@@ -146,5 +198,10 @@ form.addEventListener('submit', function(event){
   renderShifts();
 
   form.reset();
-  alert("¡Turno reservado con éxito!");
-})
+  Swal.fire({
+    title: "¡Éxito!",
+    text: "¡Turno reservado con éxito!",
+    icon: "success",
+    confirmButtonColor: "#f8a9ae",
+  });
+});
